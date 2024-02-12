81079e44c0a90c265f5c4a35a5cb4709c68c3f2f
David Bradford <davidbradforduko@yahoo.com>
2024-02-12 14:31:35 +0100

    responsive view

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      'sm': '386px',
+      // => @media (min-width: 576px) { ... }
+
+      'md': '576px',
+      // => @media (min-width: 960px) { ... }
+
+      'lg': '820px',
+      // => @media (min-width: 1440px) { ... }
+    },
     extend: {
       fontFamily:{
         inter:['var(--inter)']
