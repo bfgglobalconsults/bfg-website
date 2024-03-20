80fcc4ca971f13b6cf91a4d90d71dcc703140928
bfgglobalconsults <122263326+bfgglobalconsults@users.noreply.github.com>
2024-03-19 19:32:24 -0700

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -21,7 +21,7 @@ export default function Home() {
   }, []);
 
   function calculateTimeLeft() {
-    const difference = +new Date("2024-03-20") - +new Date();
+    const difference = +new Date("2024-03-21") - +new Date();
     let timeLeft = {};
 
     if (difference > 0) {
