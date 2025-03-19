c20aa8eddb8b3cfb6d98316e7b2a882f222944fc
David Bradford <davidbradforduko@yahoo.com>
2025-03-19 13:37:53 +0100

    staff profile

--- src/app/team/executive-assistant/page.js
+++ src/app/team/executive-assistant/page.js
@@ -35,7 +35,7 @@ const page = () => {
                 <div className="bg-white  w-[250px] p-4 shadow-md rounded-lg">
                   <h3 className="font-semibold text-lg">Elizabeth-Mary N. Okeke</h3>
                   <p className="text-[#E45F11] font-semibold">
-                                        Assistant, Projects & Service Delivery
+                                        Executive Assistant - Client Relations & People Management
 
                   </p>
                 </div>
