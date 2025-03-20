64a1b8cda506d119147e5a38bf48166cdeabeefe
David Bradford <davidbradforduko@yahoo.com>
2025-03-20 15:10:22 +0100

    new industry fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -450,7 +450,7 @@ const Header = () => {
                         <Link href="/solutions/information-technology">Technology and Software Development</Link>
                         </p>
                         <p className="text-sm ml-9 py-[20px] hover:text-[#E45F11]">
-                        <Link href="/solutions/business-strategy">Strategy and Business Transformation</Link>
+                        <Link href="/solutions/business-strategy">Strategy and Operations Transformation</Link>
                       </p>
                       <p className="text-sm ml-9 py-[20px] hover:text-[#E45F11]">
                         <Link href="/solutions/research-analytics">Research and Analytics</Link>
