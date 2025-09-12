1e77034be618840d13a977e2255244ded464efe0
David Bradford <davidbradforduko@yahoo.com>
2025-09-12 10:10:04 +0100

    manager pictures

--- src/components/counter/CounterSection.js
+++ src/components/counter/CounterSection.js
@@ -4,7 +4,7 @@ const CounterItem = dynamic(() => import('./CounterItem'), { ssr: false })
 
 const CounterSection = () => {
   return (
-    <div className=" flex-justify-center px-3 py-6 rounded-2xl">
+    <div className=" flex justify-center px-3 py-6 rounded-2xl">
       <div className="flex flex-wrap justify-center gap-6">
         <div className="flex justify-center items-center  w-[45%] lg:w-[40%] h-[200px] border border-3 border-[#F6CAB0] p-2 rounded-lg bg-[#E26015]">
           <CounterItem end={899} label="Projects Completed" />
